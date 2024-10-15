ac2d957590e4e4b69f2416ef9e7c9f1297671250
jswildcards <>
2024-10-15 17:38:01 +0800

    feat: update

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -14,15 +14,15 @@ return {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(e)
         local opts = { buffer = e.buf, noremap = true, silent = true, }
-        vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<cr>", opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", opts)
-        vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
+        vim.keymap.set("n", "<leader>gR", "<cmd>Telescope lsp_references<cr>", opts)
+        vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "<leader>gd", "<cmd>Telescope lsp_definitions<cr>", opts)
+        vim.keymap.set("n", "<leader>gi", "<cmd>Telescope lsp_implementations<cr>", opts)
         vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
         vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
         vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
         vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, opts)
       end
     })
 
